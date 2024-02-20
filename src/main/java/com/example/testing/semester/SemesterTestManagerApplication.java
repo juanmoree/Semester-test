56d772157ea4905a89b8c88b7c65e654670edef9
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2024-02-20 21:52:00 +0100

    Initial commit

---
+++ src/main/java/com/example/testing/semester/SemesterTestManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing.semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterTestManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemesterTestManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testing/semester/SemesterTestManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing.semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemesterTestManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
